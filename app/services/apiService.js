739a5c4615e7c66836fc02e382850af95ea6b376
Aram Arakelyan <100670744+aramarakelyan88@users.noreply.github.com>
2025-04-19 17:05:52 +0400

    Remove unused components

--- app/services/apiService.js
+++ app/services/apiService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://qpk88mg9-3000.euw.devtunnels.ms/api',
   headers: {
     'Content-Type': 'application/json',
   },
